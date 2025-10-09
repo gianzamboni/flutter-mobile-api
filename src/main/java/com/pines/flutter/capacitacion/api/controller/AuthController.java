13bd511055e254b0b6ac856812bd9d80fa5607d9
Gianfranco Zamboni <gianfranco.zamboni@10pines.com>
2025-10-09 13:09:08 -0300

    Removed unused variable

--- src/main/java/com/pines/flutter/capacitacion/api/controller/AuthController.java
+++ src/main/java/com/pines/flutter/capacitacion/api/controller/AuthController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegistrationDTO registrationDTO) {
